933d319b8ff90ff04813611be3c7f01fb5b4b389
Angelo <Angelodimauro91@gmail.com>
2022-10-05 12:00:59 +0200

    add Section_two

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <header_cli />
     <section_one />
+    <section_two />
   </div>
 </template>
 
 <script>
 import header_cli from '/src/components/header_cli.vue'
 import section_one from '/src/components/section_one.vue'
+import section_two from '/src/components/section_two.vue'
 
 export default {
   name: 'App',
   components: {
     header_cli,
-    section_one
+    section_one,
+    section_two,
 }
 }
 </script>
---
+++ src/components/section_two.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Ciao</div>
+</template>
+
+<script>
+        export default{
+    Name: `section_two`,
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
