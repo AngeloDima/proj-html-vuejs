6b98576bab1044a61d6ff9fd43a7f237d48ffc05
Angelo <Angelodimauro91@gmail.com>
2022-10-05 11:56:05 +0200

    Section_one (macro section color)

--- src/components/section_one.vue
+++ src/components/section_one.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
-        <p>aa</p>
-    </div>
+    <section id="section_two">
+        <div class="Sx"></div>
+        <div class="Dx"></div>
+    </section>
 </template>
 
 <script>
@@ -11,5 +12,25 @@
 </script>
 
 <style lang="scss" scoped>
-    
+    #section_two{
+        width: 100%;
+        height: 100vh;
+        background-color: rgb(236, 236, 236);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 5%;
+
+        .Sx{
+            width: 30%;
+            height: 80%;
+            background-color: red;
+        }
+
+        .Dx{
+            width: 35%;
+            height: 80%;
+            background-color: rgba(255, 0, 0, 0.582);
+        }
+    }
 </style>
