065823c7088e6516940f0b7fbbadfc7e4aa83ff5
Angelo <Angelodimauro91@gmail.com>
2022-10-05 11:42:58 +0200

    macro section (header)

--- src/components/header_cli.vue
+++ src/components/header_cli.vue
@@ -1,7 +1,21 @@
 <template>
     <section class="flex">
-        <div class="Sx"></div>
-        <div class="Dx"></div>
+
+
+
+        <div class="Sx">
+            <div class="logo"></div>
+            <div class="info-header"></div>
+        </div>
+
+        <!-- ------------------------------------- -->
+
+        <div class="Dx">
+            <div class="menù"></div>
+        </div>
+
+
+
     </section>
 </template>
 
@@ -21,6 +35,23 @@
         width: 40%;
         height: 100vh;
         background-color: rgb(0, 0, 0);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15%;
+        .logo{
+            width: 100%;
+            height: 10%;
+            background-color: aqua;
+        }
+        .info-header{
+            width: 80%;
+            height: 55%;
+            background-color: red;
+
+
+
+        }
     }
     
     .Dx{
@@ -28,6 +59,11 @@
         height: 100vh;
         background-image: url(img/slider52x.jpg);
         background-size: cover;
+        .menù{
+            width: 100%;
+            height: 10%;
+            background-color: rgba(0, 255, 255, 0.534);
+        }
     }
 
 
