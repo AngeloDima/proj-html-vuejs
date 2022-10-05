0872e5ba22837ca007b75d230cfee1ababc7df60
Angelo <Angelodimauro91@gmail.com>
2022-10-05 12:11:59 +0200

    Section_two (macro section color)

--- src/components/section_two.vue
+++ src/components/section_two.vue
@@ -1,5 +1,32 @@
 <template>
-<div>Ciao</div>
+    <section id="section_two">
+
+
+        <div class="card">
+            <div class="card-img"></div>
+            <div class="card-title"></div>
+            <div class="card-info"></div>
+            <div class="card-button"></div>
+        </div>
+
+
+        <div class="card">
+            <div class="card-img"></div>
+            <div class="card-title"></div>
+            <div class="card-info"></div>
+            <div class="card-button"></div>
+        </div>
+
+
+        <div class="card">
+            <div class="card-img"></div>
+            <div class="card-title"></div>
+            <div class="card-info"></div>
+            <div class="card-button"></div>
+        </div>
+
+
+    </section>
 </template>
 
 <script>
@@ -9,5 +36,40 @@
 </script>
 
 <style lang="scss" scoped>
+    #section_two{
+        width: 100%;
+        height: 60vh;
+        background-color: cadetblue;
 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 5%;
+
+        .card{
+            width: 20%;
+            height: 80%;
+            background-color: green;
+            .card-img{
+                width: 100%;
+                height: 35%;
+                background-color: red;
+            }
+            .card-title{
+                width: 100%;
+                height: 10%;
+                background-color: rgba(255, 0, 0, 0.699);
+            }
+            .card-info{
+                width: 100%;
+                height: 35%;
+                background-color: aqua;
+            }
+            .card-button{
+                width: 100%;
+                height: 20%;
+                background-color: rgb(0, 182, 182)     
+            }
+        }
+    }
 </style>
