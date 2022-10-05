0f54f5bd4c2158c731f7128872d95a400e52dd53
Angelo <Angelodimauro91@gmail.com>
2022-10-05 12:23:58 +0200

    Section_three (macro section color)

--- src/components/section_three.vue
+++ src/components/section_three.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="div">
-        asd
-    </div>
+    <section id="section_three">
+        <div class="Sx"></div>
+        <div class="Dx">
+            <div class="info"></div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -11,5 +14,33 @@
 </script>
 
 <style lang="scss" scoped>
+    #section_three{
+        width: 100%;
+        height: 90vh;
+        background-color: rgb(236, 236, 236);
+        display: flex;
 
+        .Sx{
+            width: 61%;
+            height: 100%;
+            background-image: url(img/slider32x.jpg);
+            background-size: cover;
+            background-position: top;
+        }
+
+        .Dx{
+            width: 39%;
+            height: 100%;
+            background-color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .info{
+                width: 80%;
+                height: 80%;
+                background-color: rgba(255, 0, 0, 0.616);
+                
+            }
+        }
+    }
 </style>
