272930dc3c10d56d6067bebcaae2d2775f068ad3
Angelo <Angelodimauro91@gmail.com>
2022-10-06 12:30:11 +0200

    section_five (macro aree)

--- src/components/section_five.vue
+++ src/components/section_five.vue
@@ -1,9 +1,6 @@
 <template>
     <section id="section_three">
-        <div class="Sx"></div>
-        <div class="Dx">
-            <div class="info"></div>
-        </div>
+        <div class="banner"></div>
     </section>
 </template>
 
@@ -16,31 +13,16 @@
 <style lang="scss" scoped>
     #section_three{
         width: 100%;
-        height: 90vh;
+        height: 60vh;
         background-color: rgb(236, 236, 236);
         display: flex;
 
-        .Sx{
-            width: 61%;
+        .banner{
+            width: 100%;
             height: 100%;
-            background-image: url(img/slider32x.jpg);
+            background-image: url(img/page52x.jpg);
             background-size: cover;
-            background-position: top;
-        }
-
-        .Dx{
-            width: 39%;
-            height: 100%;
-            background-color: black;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            .info{
-                width: 80%;
-                height: 80%;
-                background-color: rgba(255, 0, 0, 0.616);
-                
-            }
+           
         }
     }
 </style>
