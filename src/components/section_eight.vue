6a7d117435bbcca45b5ea657d2cb2d8f72597bfa
Angelo <Angelodimauro91@gmail.com>
2022-10-06 13:54:00 +0200

    section_eight restart
    add card (macro aree)

--- src/components/section_eight.vue
+++ src/components/section_eight.vue
@@ -1,23 +1,8 @@
 <template>
-    <section id="section_six">
-        <div class="title">TITOLO</div>
-        <div class="container-card">
-
-
-            <div class="card">
-                <div class="info-card"></div>
-            </div>
-
-            <div class="card">
-                <div class="info-card"></div>
-            </div>
-
-            <div class="card">
-                <div class="info-card"></div>
-            </div>
-
-
-        </div>
+    <section id="section_eight">
+        <div class="card"></div>
+        <div class="card-up"></div>
+        <div class="card"></div>
     </section>
 </template>
 
@@ -28,36 +13,24 @@
 </script>
 
 <style lang="scss" scoped>
-    #section_six{
+    #section_eight{
         width: 100%;
         height: 100vh;
-        background-color: antiquewhite;
+        background-color: aqua;
         display: flex;
-        flex-direction: column;
-        .title{
-            width: 100%;
-            height: 10%;
-            background-color: red;
+        justify-content: center;
+        align-items: center;
+        .card{
+            width: 23.5%;
+            height: 70%;
+            background-color: red
         }
-        .container-card{
-            width: 100%;
-            height: 90%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 40px;
-            .card{
-                width: 21.4%;
-                height: 70%;
-                background-color: aqua;
-                display: flex;
-                align-items: flex-end;
-                .info-card{
-                    width: 100%;
-                    height: 20%;
-                    background-color: aquamarine;
-                }
-            }
+        .card-up{
+            width: 23.4%;
+            height: 70%;
+            background-color: rgb(0, 192, 0);
+            position: relative;
+            bottom: 5%;
         }
-    } 
+    }
 </style>
