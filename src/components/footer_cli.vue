eece07bbb4d024a7740009106d4dc2712f949a32
Angelo <Angelodimauro91@gmail.com>
2022-10-07 09:30:59 +0200

    footer (macro aree)

--- src/components/footer_cli.vue
+++ src/components/footer_cli.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        wela
-    </div>
+    <section id="footer">
+        <div class="box-info">
+            <div class="info"></div>
+        </div>
+    </section>
 </template>
 
 
@@ -12,6 +14,26 @@
 </script>
 
 <style lang="scss" scoped>
-
+    #footer{
+        width: 100%;
+        height: 70vh;
+        background-image: url(img/footer2x.jpg);
+        background-size: cover;
+        background-position: bottom;
+        .box-info{
+            width: 40%;
+            height: 100%;
+            background-color: rgba(255, 0, 0, 0.301);
+            margin: auto;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .info{
+                width: 100%;
+                height: 60%;
+                background-color: rgba(255, 255, 255, 0.349);
+            }
+        }
+    }
 </style>
 
