f9e4c6dd3d5736be560dfc70cae0f1699150a71a
Angelo <Angelodimauro91@gmail.com>
2022-10-09 19:48:54 +0200

    section_six end

--- src/components/section_six.vue
+++ src/components/section_six.vue
@@ -1,19 +1,31 @@
 <template>
     <section id="section_six">
-        <div class="title">TITOLO</div>
+        
         <div class="container-card">
 
-
+            <div class="title"><h2>Latest news & promotions</h2></div>
             <div class="card">
-                <div class="info-card"></div>
+                <img src="./img/blog92x-1200x1375.jpg" alt="">
+                <div class="info-card">
+                    <h3>THE BEST COFFEE IN TOWN</h3>
+                    <p>By admin January 7th, 2020 Categories: News</p>
+                </div>
             </div>
 
             <div class="card">
-                <div class="info-card"></div>
+                <img src="./img/blog72x-1200x1375.jpg" alt="">
+                <div class="info-card">
+                    <h3>DISCOVER OUR NEW MENU</h3>
+                    <p>By admin January 7th, 2020 Categories: News</p>
+                </div>
             </div>
 
             <div class="card">
-                <div class="info-card"></div>
+                <img src="./img/blog82x-1200x1375.jpg" alt="">
+                <div class="info-card">
+                    <h3>WE NOW ACCEPT SQUARE!</h3>
+                    <p>By admin January 7th, 2020 Categories: News</p>
+                </div>
             </div>
 
 
@@ -30,32 +42,52 @@
 <style lang="scss" scoped>
     #section_six{
         width: 100%;
-        height: 100vh;
-        background-color: antiquewhite;
+        height: 110vh;
+        // background-color: antiquewhite;
         display: flex;
         flex-direction: column;
-        .title{
-            width: 100%;
-            height: 10%;
-            background-color: red;
-        }
+        position: relative;
+  
         .container-card{
             width: 100%;
-            height: 90%;
+            height: 100%;
             display: flex;
             justify-content: center;
             align-items: center;
             gap: 40px;
+            // background-color: red;
+            position: relative;
+            top: 60px;
+            
+            .title{
+                position: absolute;
+                top: 0;
+                padding-top: 25px;
+                // background-color: red;
+                
+             }
             .card{
                 width: 21.4%;
-                height: 70%;
-                background-color: aqua;
+                height: 75%;
+                //  background-color: aqua;
                 display: flex;
                 align-items: flex-end;
+                position: relative;
+                img{
+                    width: 100%;
+                    height: 80%;
+                    position: absolute;
+                    top: 0;
+                }
                 .info-card{
                     width: 100%;
                     height: 20%;
-                    background-color: aquamarine;
+                    //  background-color: aquamarine;
+                    display: flex;
+                    align-items: center;
+                    flex-direction: column;
+                    gap: 15px;
+                    padding-top: 25px;
                 }
             }
         }
