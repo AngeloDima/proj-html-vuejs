638a802be2bfa1f45a35d9b44f3336deb32acf8e
Angelo <Angelodimauro91@gmail.com>
2022-10-09 19:18:51 +0200

    section_five end

--- src/components/section_five.vue
+++ src/components/section_five.vue
@@ -1,6 +1,8 @@
 <template>
     <section id="section_three">
-        <div class="banner"></div>
+        <div class="banner">
+            <div class="info"><h3>KUNK PAO CHICKEN - $32</h3></div>
+        </div>
     </section>
 </template>
 
@@ -22,7 +24,14 @@
             height: 100%;
             background-image: url(img/page52x.jpg);
             background-size: cover;
-           
+           display: flex;
+           justify-content: center;
+           align-items: flex-end;
+           .info > h3{
+            color: white;
+            position: relative;
+            bottom: 30px;
+           }
         }
     }
 </style>
