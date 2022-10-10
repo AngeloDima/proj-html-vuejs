36b007d89a7031dd77ab929f87e586746a92d364
Angelo <Angelodimauro91@gmail.com>
2022-10-10 11:13:10 +0200

    section_five details

--- src/components/section_five.vue
+++ src/components/section_five.vue
@@ -13,24 +13,38 @@
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+Telugu:wght@300&family=Oswald&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
+
+h3{
+    font-family: 'Noto Serif Telugu', serif;
+}
+
+
+
+
+
+
     #section_three{
         width: 100%;
         height: 60vh;
         background-color: rgb(236, 236, 236);
         display: flex;
+        margin: 60px 0px 10px 0px;
 
         .banner{
             width: 100%;
             height: 100%;
             background-image: url(img/page52x.jpg);
+            background-position: bottom;
             background-size: cover;
-           display: flex;
-           justify-content: center;
-           align-items: flex-end;
+            display: flex;
+            justify-content: center;
+            align-items: flex-end;
            .info > h3{
             color: white;
             position: relative;
-            bottom: 30px;
+            bottom: 20px;
            }
         }
     }
